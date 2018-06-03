9e93c8db9c3112bde2bb16b1dd75fbaa1df98a91
hwalrus <39668365+hwalrus@users.noreply.github.com>
2018-06-03 10:08:27 +0800

    add chalk and move start scripts to buildscripts

--- buildScripts/app.js
+++ buildScripts/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './src/index.html'));
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err) {
